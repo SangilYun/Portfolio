e8700bd6cb44da4715979428b336cebf012dee9a
sangil <nasuler@gmail.com>
2020-10-03 18:39:12 +0900

    feat: add navbar component

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="d-flex justify-content-between">
+      <div>sangil yun</div>
+      <div className="d-flex">
+        <div>about</div>
+        <div className="ml-4">projects</div>
+        <div className="ml-4">blog</div>
+        <div className="ml-4">contact</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

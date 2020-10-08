6c49f486b1b1d072be20343ba7406af6aa769a8e
Sangil Yun <sangilyun@Sangils-MacBook-Air.local>
2020-10-08 22:00:51 +0900

    style: change nav bar styles

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,20 +4,30 @@ const Navbar = () => {
   return (
     <nav className={`${styles.root} d-flex justify-content-between`}>
       <div>
-        <Link href="/">sangil yun</Link>
+        <Link href="/">
+          <a className={styles.logo}>Sangil Yun</a>
+        </Link>
       </div>
       <div className="d-flex">
         <div>
-          <Link href="/about">about</Link>
+          <Link href="/about">
+            <a className={styles.link}>about</a>
+          </Link>
         </div>
         <div className="ml-4">
-          <Link href="/projects">projects</Link>
+          <Link href="/projects">
+            <a className={styles.link}>projects</a>
+          </Link>
         </div>
         <div className="ml-4">
-          <Link href="/blog">blog</Link>
+          <Link href="/blog">
+            <a className={styles.link}>blog</a>
+          </Link>
         </div>
         <div className="ml-4">
-          <Link href="/contact">contact</Link>
+          <Link href="/contact">
+            <a className={styles.link}>contact</a>
+          </Link>
         </div>
       </div>
     </nav>

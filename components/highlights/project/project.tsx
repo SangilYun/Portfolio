0b2cb4695af601ac6af77521449780168cf08356
Sangil Yun <sangilyun@Sangils-MacBook-Air.local>
2020-10-08 22:00:27 +0900

    add project component

---
+++ components/highlights/project/project.tsx
@@ -0,0 +1,11 @@
+import HighlightTitle from "../highlighTitle";
+
+const Project = () => {
+  return (
+    <div>
+      <HighlightTitle>Highlighted Projects</HighlightTitle>
+    </div>
+  );
+};
+
+export default Project;

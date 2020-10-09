61aaaafe1da37f1097552e57d0e27895a6d1b607
SangilYun <nasuler@gmail.com>
2020-10-09 23:32:27 +0900

    style: fix background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,11 @@ import Container from "components/container";
 import Profile from "components/profile";
 import Project from "components/highlights/project";
 import Blog from "components/highlights/blog";
-import styles from "./styles.module.css";
 // import styles from "../styles/Home.module.css";
 
 export default function App() {
   return (
-    <div className={styles.root}>
+    <div>
       <Head>
         <title> Sangil Yun</title>
         <link rel="icon" href="/favicon.ico" />

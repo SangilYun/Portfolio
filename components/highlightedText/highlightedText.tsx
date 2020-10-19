eee42930486263c2767d3eaeb99b4d27f5e719e2
SangilYun <nasuler@gmail.com>
2020-10-19 23:33:01 +0900

    add href in HighlightedText component

--- components/highlightedText/highlightedText.tsx
+++ components/highlightedText/highlightedText.tsx
@@ -1,7 +1,16 @@
 import styles from "./styles.module.css";
+import { useRouter } from "next/router";
 
-const HighlightedText = (props: { children: React.ReactNode }) => {
-  return <div className={styles.root}> {props.children}</div>;
+const HighlightedText = (props: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  const router = useRouter();
+  return (
+    <div className={styles.root} onClick={() => router.push(props.href)}>
+      {props.children}
+    </div>
+  );
 };
 
 export default HighlightedText;

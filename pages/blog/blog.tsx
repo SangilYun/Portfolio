5764da52d8409143d7a07dfe71bb6d9afcf14c46
SangilYun <nasuler@gmail.com>
2020-10-19 23:37:28 +0900

    add Blog page

--- pages/blog/blog.tsx
+++ pages/blog/blog.tsx
@@ -1,11 +1,62 @@
 import Layout from "components/layout";
 import Container from "components/container";
+import BlogCard from "components/blogCard";
+import fetchContentful from "utils/fetchContentful";
+import styles from "./styles.module.css";
 
-const Blog = () => {
+interface BlogProps {
+  blogPosts: Array<{
+    sys: { id: string };
+    title: string;
+    description: { json: Array<any> }; //TODO: add description type
+  }>;
+}
+const Blog = ({ blogPosts }: BlogProps) => {
   return (
     <Layout>
-      <Container>blog page</Container>
+      <Container>
+        <div className={styles.root}>
+          <header className={styles.pageTitle}>Blog</header>
+          <div className={styles.pageDescription}>
+            Here are some blog posts I've written
+          </div>
+          <div>
+            {blogPosts.map((post, index) => (
+              <BlogCard
+                id={post.sys.id}
+                key={index}
+                title={post.title}
+                description={post.description.json}
+              />
+            ))}
+          </div>
+        </div>
+      </Container>
     </Layout>
   );
 };
 export default Blog;
+
+export const getStaticProps = async () => {
+  const {
+    data: {
+      blogPostsCollection: { items },
+    },
+  } = await fetchContentful(`query{
+    blogPostsCollection{
+      items{
+        sys{
+          id
+        },
+        title,
+        description{
+          json
+        }
+      }
+    }
+  }`);
+
+  return {
+    props: { blogPosts: items },
+  };
+};
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,1 +1,1 @@
-export { default } from "./blog";
+export { default, getStaticProps } from "./blog";

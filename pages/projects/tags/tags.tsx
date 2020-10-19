b28240b0f28aca1ee712ce4ebe5b7593181adbe2
SangilYun <nasuler@gmail.com>
2020-10-19 21:20:37 +0900

    add Tags component

---
+++ pages/projects/tags/tags.tsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.css";
+import cn from "classnames";
+const Tags = ({
+  tags,
+  selectedTags,
+  onClick,
+}: {
+  tags: string[];
+  selectedTags?: boolean;
+  onClick: (tag: string) => void;
+}) => {
+  const onTagClick = (tag: string) => {
+    onClick(tag);
+  };
+  return (
+    <div className={styles.root}>
+      {tags.map((tag) => (
+        <span
+          key={tag}
+          className={cn(styles.tag, { [styles.selectedTag]: selectedTags })}
+          onClick={() => onTagClick(tag)}
+        >{`#${tag} `}</span>
+      ))}
+    </div>
+  );
+};
+export default Tags;

aa2131c28aa1f029e5c1e467981fe5cde0ee13d8
SangilYun <nasuler@gmail.com>
2020-10-19 23:45:14 +0900

    add onClick in footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,4 @@
 import Container from "components/container";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./styles.module.css";
@@ -14,14 +13,19 @@ const Footer = () => {
           <FontAwesomeIcon
             className={cn(styles.icon, "fab fa-linkedin fa-lg")}
             icon={["fab", "github"]}
+            onClick={() => window.open("https://github.com/SangilYun")}
           />
           <FontAwesomeIcon
             className={cn(styles.icon, "fab fa-linkedin fa-lg")}
             icon={["fab", "linkedin"]}
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/sangil-yun-37452a103/")
+            }
           />
           <FontAwesomeIcon
             className={cn(styles.icon, "fab fa-linkedin fa-lg")}
             icon={["fab", "medium"]}
+            onClick={() => window.open("https://medium.com/@sangilyun")}
           />
         </div>
       </div>

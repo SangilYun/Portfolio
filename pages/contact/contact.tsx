7a557fa27315080965d763a5fea8ef30af6aa570
SangilYun <nasuler@gmail.com>
2020-10-20 13:52:17 +0900

    add contact page form

--- pages/contact/contact.tsx
+++ pages/contact/contact.tsx
@@ -4,7 +4,36 @@ import Container from "components/container";
 const Contact = () => {
   return (
     <Layout>
-      <Container>contact page</Container>
+      <Container>
+        contact page
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          {/* You still need to add the hidden input with the form name to your JSX form */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </Container>
     </Layout>
   );
 };

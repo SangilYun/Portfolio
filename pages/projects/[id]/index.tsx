234455dc972194c3806e11f2a41de6f213950ebd
SangilYun <nasuler@gmail.com>
2020-11-02 18:27:58 +0900

    add links and hyperlink style

--- pages/projects/[id]/index.tsx
+++ pages/projects/[id]/index.tsx
@@ -3,6 +3,7 @@ import Container from "components/container";
 import ImageList from "components/imageList";
 import fetchContentful from "utils/fetchContentful";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { INLINES } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import styles from "./styles.module.css";
@@ -15,7 +16,24 @@ interface ProjectProps {
   };
   sys: { id: string };
   title: string;
+  github: string;
+  demo: string;
+  tags: string[];
 }
+const OPTIONS = {
+  renderNode: {
+    [INLINES.HYPERLINK]: (node: any, children: React.ReactNode) => {
+      return (
+        <span
+          onClick={() => window.open(node.data.uri)}
+          style={{ color: "#4299e1", cursor: "pointer" }}
+        >
+          {children}
+        </span>
+      );
+    },
+  },
+};
 const Project = ({ post }: { post: Array<ProjectProps> }) => {
   if (!post) {
     return null;
@@ -23,32 +41,54 @@ const Project = ({ post }: { post: Array<ProjectProps> }) => {
   const {
     title,
     description,
+    github,
+    demo,
+    tags,
     imagesCollection: { items: images },
   } = post[0];
+  console.log(post[0]);
   return (
     <Layout>
       <Container>
         <header className={styles.header}>
           <h1>{title}</h1>
           <aside className={styles.buttons}>
-            <span className={styles.demo_btn}>
-              <FontAwesomeIcon
-                className={" m-0 mr-2 fas-2x"}
-                icon={["fas", "desktop"]}
-              />
-              demo
-            </span>
-            <span className={styles.github_btn}>
-              <FontAwesomeIcon
-                className={"m-0 mr-2 fa-lg"}
-                icon={["fab", "github"]}
-              />
-              github
-            </span>
+            {demo && (
+              <span
+                className={styles.demo_btn}
+                onClick={() => {
+                  window.open(demo);
+                }}
+              >
+                <FontAwesomeIcon
+                  className={" m-0 mr-2 fas-2x"}
+                  icon={["fas", "desktop"]}
+                />
+                demo
+              </span>
+            )}
+            {github && (
+              <span
+                className={styles.github_btn}
+                onClick={() => window.open(github)}
+              >
+                <FontAwesomeIcon
+                  className={"m-0 mr-2 fa-lg"}
+                  icon={["fab", "github"]}
+                />
+                github
+              </span>
+            )}
           </aside>
         </header>
         <main className={styles.description}>
-          {documentToReactComponents(description?.json)}
+          {documentToReactComponents(description?.json, OPTIONS)}
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              style={{ marginRight: "10px", color: "#4a5568" }}
+            >{`#${tag}`}</span>
+          ))}
         </main>
         <ImageList images={images} />
       </Container>
@@ -77,6 +117,9 @@ export const getStaticProps = async ({
         	json
       	},
         date,
+        github,
+        demo,
+        tags,
         imagesCollection{
           items{
             title,

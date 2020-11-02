89b438a654760c28eea586e592f4df55058e7ec9
SangilYun <nasuler@gmail.com>
2020-11-02 18:26:18 +0900

    add link to introduction

--- components/profile/introduction/introduction.tsx
+++ components/profile/introduction/introduction.tsx
@@ -1,5 +1,8 @@
 import styles from "./styles.module.css";
+import { useRouter } from "next/router";
+
 const Introduction = () => {
+  const router = useRouter();
   return (
     <div>
       <div className={styles.greeting}>HI, I'M SANGIL</div>
@@ -8,8 +11,30 @@ const Introduction = () => {
         developement using React, Mobx, flow and more.
       </div>
       <div className={styles.intro}>
-        Here you can see my selected projects I’ve done as well as my blog. You
-        can also contact me or view my resume.
+        Here you can see my
+        <span className={styles.text} onClick={() => router.push("/projects")}>
+          {` selected projects `}
+        </span>
+        I’ve done as well as my
+        <span className={styles.text} onClick={() => router.push("/projects")}>
+          {` blog`}
+        </span>
+        . You can also
+        <span className={styles.text} onClick={() => router.push("/contact")}>
+          {` contact me `}
+        </span>
+        or
+        <span
+          className={styles.text}
+          onClick={() =>
+            window.open(
+              "https://drive.google.com/file/d/15e7BjPuVvHb8tkf1L7RHJwi1hmo64W-s/view?usp=sharing"
+            )
+          }
+        >
+          {` view my resume`}
+        </span>
+        .
       </div>
     </div>
   );
